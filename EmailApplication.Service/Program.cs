1f587d321117955b32a019a8e063a281262639c3
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-09-06 00:04:53 +0530

    Subject and body added

--- EmailApplication.Service/Program.cs
+++ EmailApplication.Service/Program.cs
@@ -14,7 +14,21 @@ namespace EmailApplication.Service
            MimeMessage message = new MimeMessage();
 
             //add the sender info that will appear in the email message
+
             message.From.Add(new MailboxAddress("Ashen Dunusinghe", "theeventprojectg259@gmail.com"));
+
+            //add the receiver email address
+
+            message.To.Add(MailboxAddress.Parse("avdunusinghe@gmail.com"));
+
+            //add the message Subject
+            message.Subject = "This Testing Email";
+
+            //add the message body as plain text String passed to the TextPart indicates that it's plain text 
+            message.Body = new TextPart("plain")
+            {
+                Text = @"This email send by using SMTP Server"
+            };
         }
     }
 }
